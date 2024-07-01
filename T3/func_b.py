c52d0598613287833a8125831534ad5d21b125cc
HCAlles <110581234+HCAlles@users.noreply.github.com>
2024-07-01 19:40:42 -0300

    Add files via upload

--- T3/func_b.py
+++ T3/func_b.py
@@ -1,12 +1,54 @@
 from main import*
 import sqlite3
+import sys
 
 con=sqlite3.connect("tutorial.db")
 cur=con.cursor()
-dados_senha=cur.execute("SELECT senha from registro where nome='test1'")
+dados_senha=cur.execute("SELECT senha from registro where nome='test2'")
 for linha in dados_senha.fetchall():
         senhas=linha
 con.commit()
+
+
+senha_digitada=""
+app = QtWidgets.QApplication(sys.argv)
+Dialog = QtWidgets.QDialog()
+ui = Ui_Dialog()
+ui.setupUi(Dialog)
+
+def func_():
+    print("testando")
+def func_senha():
+    print(str(ui.lineEdit_senha.text()))
+
+def func_bLimpar():
+     print("bLimpar clicado")
+     ui.lineEdit_senha.setText("")
+     ui.lineEdit_funcao.setText("")
+     ui.lineEdit_nome.setText("")
+def func_inicia_ui():
+     global ui
+     global app
+     global Dialog
+     ui.bCadastrar.clicked.connect(func_senha)
+     ui.b0.clicked.connect(func_b0)
+     ui.b1.clicked.connect(func_b1)
+     ui.b2.clicked.connect(func_b2)
+     ui.b3.clicked.connect(func_b3)
+     ui.b4.clicked.connect(func_b4)
+     ui.b5.clicked.connect(func_b5)
+     ui.b6.clicked.connect(func_b6)
+     ui.b7.clicked.connect(func_b7)
+     ui.b8.clicked.connect(func_b8)
+     ui.b9.clicked.connect(func_b9)
+     
+     ui.bApagar.clicked.connect(func_bApagar)
+     ui.bCadastrar.clicked.connect(func_bCadastrar)
+     ui.bEnter.clicked.connect(func_bEnter)
+     ui.bLimpar.clicked.connect(func_bLimpar)
+     Dialog.show()
+     sys.exit(app.exec_())
+
 
 def func_b0():
      print("b0 clicado")
@@ -51,7 +93,21 @@ def func_b9():
 
 def func_bCadastrar():
      global senha_digitada
+     global ui
+     con=sqlite3.connect("tutorial.db")
+     cur=con.cursor()
+     teste = 'terroso'
      print("bCadastrar clicado")
+     ui.label_senha.setText("Senha: "+ui.lineEdit_senha.text())
+     ui.label_funcao.setText("Funcao: "+ui.lineEdit_funcao.text())
+     ui.label_nome.setText("Nome: "+ui.lineEdit_nome.text())
+     string_cadastro="INSERT into registro values "+"('"+ui.lineEdit_nome.text()+"','"+ui.lineEdit_senha.text()+"','"+ui.lineEdit_funcao.text()+"')"
+     print("string cadastro é: "+string_cadastro)
+     cur.execute(string_cadastro)
+     con.commit()
+     
+     
+     
 def func_bApagar():
      global senha_digitada
      print("bApagar clicado")
@@ -65,7 +121,6 @@ def func_bEnter():
      if senha_digitada==senhas[0]:
           print("senha correta")
      else: 
-          
           print("Erro")
           print(senha_digitada+" diferente de "+senhas[0])
 
--- T3/main.py
+++ T3/main.py
@@ -2,23 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from ui import Ui_Dialog
 from func_b import *
 import sqlite3
-senha_digitada=""
-def func_():
-    print("testando")
-def func_senha():
-    print(str(ui.lineEdit_senha.text()))
 
-def func_bLimpar():
-     print("bLimpar clicado")
-     ui.lineEdit_senha.setText("")
-     ui.lineEdit_funcao.setText("")
-     ui.lineEdit_nome.setText("")
-     
-def func_bCadastrar():
-     print("bCadastrar clicado")
-     ui.label_senha.setText("Senha: "+ui.lineEdit_senha.text())
-     ui.label_funcao.setText("Funcao: "+ui.lineEdit_funcao.text())
-     ui.label_nome.setText("Nome: "+ui.lineEdit_nome.text())
 
      
 if __name__ == "__main__":
@@ -53,29 +37,5 @@ if __name__ == "__main__":
     
     con.commit()
     '''
-
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    ui.bCadastrar.clicked.connect(func_senha)
-    ui.b0.clicked.connect(func_b0)
-    ui.b1.clicked.connect(func_b1)
-    ui.b2.clicked.connect(func_b2)
-    ui.b3.clicked.connect(func_b3)
-    ui.b4.clicked.connect(func_b4)
-    ui.b5.clicked.connect(func_b5)
-    ui.b6.clicked.connect(func_b6)
-    ui.b7.clicked.connect(func_b7)
-    ui.b8.clicked.connect(func_b8)
-    ui.b9.clicked.connect(func_b9)
+    func_inicia_ui()
     
-    ui.bApagar.clicked.connect(func_bApagar)
-    ui.bCadastrar.clicked.connect(func_bCadastrar)
-    ui.bEnter.clicked.connect(func_bEnter)
-    ui.bLimpar.clicked.connect(func_bLimpar)
-    
-    
-    
-    Dialog.show()
-    sys.exit(app.exec_())
